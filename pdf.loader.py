39890b38b3fc33f7063c57b5cbbc6eef5d7ff9a2
Atharva Patil <atharvaspatil2005@gmail.com>
2026-03-08 17:29:20 +0530

    limit chunk size:

--- pdf.loader.py
+++ pdf.loader.py
@@ -14,3 +14,10 @@ def load_pdf(file_path):
             text += extracted + "\n"
 
     return text
+def chunk_text(text,chunk_size=500):        ##limited chunk size
+    words=text.split()
+    chunks=[]
+    for i in range(0,len(words),chunk_size):
+        chunk=" ".join(words[i:i + chunk_size])
+        chunks.append(chunk)
+        return chunks
