be2610fc4045cb948288c91ef2eb682d7be5c63c
Atharva Patil <atharvaspatil2005@gmail.com>
2026-03-08 17:49:21 +0530

    upadate embeddings to convert text to embedding vector

--- embedding.py
+++ embedding.py
@@ -1,8 +1,11 @@
 from sentence_transformers import SentenceTransformer
+import numpy as np
 
 # Load embedding model
 
 embedder = SentenceTransformer("all-MiniLM-L6-v2")
 
-def embed_text(texts):
-    return embedder.encode(texts)
+def embed_chunks(chunks):
+    embeddings=embedder.encode(chunks)
+
+    return np.array(embeddings).astype("float32")
