51f763b86468be5b90fd08432580d9982738d52e
Atharva Patil <atharvaspatil2005@gmail.com>
2026-03-08 18:20:04 +0530

    vector store returns distance and indices

--- vector_store.py
+++ vector_store.py
@@ -3,10 +3,12 @@ import numpy as np
 
 class VectorStore:
     def __init__(self, embeddings):
+        embeddings=np.arrary(embeddings).astype("float32")
         self.dimension = embeddings.shape[1]
         self.index = faiss.IndexFlatL2(self.dimension)
         self.index.add(embeddings)
 
     def search(self, query_vector, k=2):
+        query_vector=np.array(query_vector).astype("float32")
         distances, indices = self.index.search(query_vector, k)
-        return indices[0]
+        return distances, indices[0] 
