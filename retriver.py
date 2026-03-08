935254b8c151ce0516e9916518a21a0b4e48f6c1
Atharva Patil <atharvaspatil2005@gmail.com>
2026-03-08 18:24:50 +0530

    upadted retriver to return indices and distances

--- retriver.py
+++ retriver.py
@@ -2,7 +2,10 @@ import numpy as np
 from embedding import embed_text
 
 def retrieve(query, vector_store, chunks, k=2):
+    #query->embedding
     query_vector = embed_text([query])
     query_vector = np.array(query_vector).astype("float32")
-    indices = vector_store.search(query_vector, k)
+    #searches vectore store
+    distances,indices=vector_store.search(query_vector,k)
+    #reuturns mathcing chunks
     return [chunks[i] for i in indices]
