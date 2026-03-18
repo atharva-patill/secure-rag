8e5f0a63343620f683c074520671df321a3d9087
Atharva Patil <atharvaspatil2005@gmail.com>
2026-03-18 16:24:30 +0530

    added comments

--- tui.py
+++ tui.py
@@ -1,31 +1,31 @@
-from rich.console import Console
-from rich.panel import Panel
-from rich.prompt import Prompt
+from rich.console import Console    #for styled outputs
+from rich.panel import Panel        #panel draws boxes
+from rich.prompt import Prompt      #for inputs
 
 from rag_pipeline import rag_answer
 
-console=Console()
-
-def run_chat():
-    console.print(
-        Panel(
+console=Console()   #replacing print (print=>basic python output)
+                    #console.print => styled ui
+def run_chat(): #main fn
+    console.print(#renders the box
+        Panel(#creates box
             "Secure RAG Terminal Chat\nType 'exit' to quit",
             style="bold green"
         )
     )
-    while True:
+    while True:#chat is in infinite loop
         #user input 
         user_input=Prompt.ask("\n[bold cyan]You[/bold cyan]")
-        if user_input.lower()in["exit","quit"]:
+        if user_input.lower()in["exit","quit"]:#exit/quit to exit the loop
             console.print("\n Exiting...\n",style="bold red")
             break
 
         #show buffering/thinking
-        console.print("[yellow] thinking..[/yellow]")
+        console.print("[yellow] Thinking..[/yellow]")
 
         #response
-        response=rag_answer(user_input)
-        if not isinstance(response,str):
+        response=rag_answer(user_input)#calls rag_pipeline
+        if not isinstance(response,str):#converting genrator to string
             response="".join(response)
         #print response
         console.print(
