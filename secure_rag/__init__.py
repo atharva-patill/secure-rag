ef33cbcf978b8909fc045948928bd13511e974c7
Atharva Patil <atharvaspatil2005@gmail.com>
2026-03-20 08:32:51 +0530

    defined public api and exposed internal modules

--- secure_rag/__init__.py
+++ secure_rag/__init__.py
@@ -1,4 +1,4 @@
 #import fn from pipline
 from .rag_pipeline import build_rag, rag_answer
 #users can import from secure_rag
-__all__ = ["build_rag", "rag_answer"]
+__all__ = ["build_rag", "rag_answer"]#defined pulbic api's
