14e40005b9c1d694465a138bfa476816ccdd4652
Atharva Patil <atharvaspatil2005@gmail.com>
2026-03-20 18:15:04 +0530

    embed fn  into float 32 format

--- secure_rag/embedding.py
+++ secure_rag/embedding.py
@@ -12,7 +12,7 @@ def get_embedder(model_name: str = _DEFAULT_MODEL_NAME) -> SentenceTransformer:
     return _embedder
 
 
-def embed_chunks(chunks):
+def embed_chunks(chunks):#embedd chunks into float32 format
     embedder = get_embedder()
     embeddings = embedder.encode(chunks)
     return np.array(embeddings).astype("float32")
