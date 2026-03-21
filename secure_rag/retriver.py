ba257adac977c014403f0d668ddd525e7ca67167
Atharva Patil <atharvaspatil2005@gmail.com>
2026-03-21 15:39:46 +0530

    correct formatting for faiss vector == float 32

--- secure_rag/retriver.py
+++ secure_rag/retriver.py
@@ -2,10 +2,10 @@ import numpy as np
 from .embedding import embed_chunks#from embedding module
 
 def retrieve(query, vector_store, chunks, k=2):#params==> user input,faiss index,original text chunks,number of results
-    #query->embedding
+    #query->vector
     query_vector = embed_chunks([query])
-    query_vector = np.array(query_vector).astype("float32")
+    query_vector = np.array(query_vector).astype("float32")#ensures correct format(beacuse faiss require float 32 vectors)
     #searches vectore store
     distances,indices=vector_store.search(query_vector,k)
-    #reuturns matching chunks
+    #reuturns matching indices to  chunks
     return [chunks[i] for i in indices]
