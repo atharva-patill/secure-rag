40b367abbbc6fe3aba5239c91dd860ae6ccda2a7
Atharva Patil <atharvaspatil2005@gmail.com>
2026-03-21 15:19:37 +0530

    used regex for pattern based masking for phone number and email

--- secure_rag/masker.py
+++ secure_rag/masker.py
@@ -1,6 +1,6 @@
 import re #regular expression module for pattern matching
 
-def mask_text(text: str) -> str:
-    text = re.sub(r'\S+@\S+', '[EMAIL_MASKED]', text)
-    text = re.sub(r'\b\d{10}\b', '[PHONE_MASKED]', text)
-    return text
+def mask_text(text: str) -> str:#input == text 
+    text = re.sub(r'\S+@\S+', '[EMAIL_MASKED]', text)#regex for email masking 
+    text = re.sub(r'\b\d{10}\b', '[PHONE_MASKED]', text)#find & replace 10 digits pattern matched digit 
+    return text#output == masked verison 
