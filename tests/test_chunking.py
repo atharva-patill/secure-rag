fa027393b4178897692064ca115d97e6bc6a4797
Atharva Patil <atharvaspatil2005@gmail.com>
2026-03-30 08:59:09 +0530

    comment : chunking'

--- tests/test_chunking.py
+++ tests/test_chunking.py
@@ -1,7 +1,7 @@
 from secure_rag.pdf_loader import chunk_text
 
 
-def test_chunking_basic():
+def test_chunking_basic():#chunk test fn 
     text = "word " * 1000  # 1000 words
     chunks = chunk_text(text, chunk_size=200)
 
