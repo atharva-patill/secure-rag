6b952315df9debed2c60416595c8f10f2c418f88
Atharva Patil <atharvaspatil2005@gmail.com>
2026-04-15 09:34:33 +0530

    spacy for ner recognition

--- secure_rag/masker.py
+++ secure_rag/masker.py
@@ -1,5 +1,16 @@
+from pyexpat import model
 import re #regular expression module for pattern matching
+import os
+_nlp = None
 
+def _get_nlp():
+    global _nlp
+    if _nlp is None:
+        import spacy
+        model = os.getenv("SPACY_MODEL","en_core_web_sm")
+        _nlp=spacy.load(model)
+        return _nlp
+        
 def mask_text(text: str) -> str:#input == text 
     text = re.sub(r'\S+@\S+', '[EMAIL_MASKED]', text)#regex for email masking 
     text = re.sub(r'\b\d{10}\b', '[PHONE_MASKED]', text)#find & replace 10 digits pattern matched digit 
